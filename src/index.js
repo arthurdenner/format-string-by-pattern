8c464de468c06d4f2c6a2b56e09b01a711398fba
Arthur Denner <arthurdenner7@gmail.com>
2018-08-24 17:05:32 -0300

    Reducing a few bytes by declaring variables inside loop

--- src/index.js
+++ src/index.js
@@ -12,17 +12,12 @@ function formatStringByPattern(pattern, value) {
   const [firstSeparator] = separators;
 
   if (pattern.startsWith(firstSeparator)) {
-    let curBlockSize;
-    let beforeSlice;
-    let afterSlice;
-    let nextResult;
-
     const afterReduce = separators.reduce(
       (acc, cur, index) => {
-        curBlockSize = blockSizes[index];
-        beforeSlice = acc.value.slice(0, curBlockSize);
-        afterSlice = acc.value.slice(curBlockSize);
-        nextResult = beforeSlice
+        const curBlockSize = blockSizes[index];
+        const beforeSlice = acc.value.slice(0, curBlockSize);
+        const afterSlice = acc.value.slice(curBlockSize);
+        const nextResult = beforeSlice
           ? acc.result.concat(cur, beforeSlice)
           : acc.result;
 
@@ -40,19 +35,13 @@ function formatStringByPattern(pattern, value) {
     return afterReduce.result.slice(0, pattern.length);
   }
 
-  let curSeparator;
-  let replace;
-  let curSlice;
-  let curRegex;
-  let curValue;
-
   const afterReduce = blockSizes.reduce(
     (acc, cur, index) => {
-      curSeparator = separators[index] || '';
-      replace = `$1${curSeparator}$2`;
-      curSlice = cur + acc.prevSlice + acc.prevSeparator.length;
-      curRegex = new RegExp(`(.{${curSlice}})(.)`);
-      curValue = acc.value.replace(curRegex, replace);
+      const curSeparator = separators[index] || '';
+      const replace = `$1${curSeparator}$2`;
+      const curSlice = cur + acc.prevSlice + acc.prevSeparator.length;
+      const curRegex = new RegExp(`(.{${curSlice}})(.)`);
+      const curValue = acc.value.replace(curRegex, replace);
 
       return {
         prevSeparator: curSeparator,
